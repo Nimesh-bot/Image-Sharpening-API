91b2f68deb3d8b4707e3a1e2c51c6e29c8515691
Nimesh Shakya <somit409@gmail.com>
2023-02-07 20:40:25 +0545

    Initial commit

---
+++ app.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI, Response
+from fastapi import FastAPI, UploadFile
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+import cv2
+import numpy as np
+import time
+import os
+
+app = FastAPI()
+app.mount("/output", StaticFiles(directory="output"), name="output")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/sharpen")
+async def sharpen_image(file: UploadFile):
+    # Reading the image from the request
+    image_file = await file.read()
+    img = cv2.imdecode(np.frombuffer(image_file, np.uint8), cv2.IMREAD_UNCHANGED)
+
+    # Gaussian kernel
+    gaussian_blur = cv2.GaussianBlur(img, (5,5), 2, 2)
+
+    # Sharpening using addWeighted()
+    sharpened = cv2.addWeighted(img, 7.5, gaussian_blur, -6.5, 0)
+
+    # Create a unique folder based on the current time
+    folder_name = str(int(time.time()))
+    os.makedirs('./output/' + folder_name, exist_ok=True)
+
+    # Save the original and sharpened images in the folder
+    cv2.imwrite('./output/' + folder_name + '/original.jpg', img)
+    cv2.imwrite('./output/' + folder_name + '/sharpened.jpg', sharpened)
+
+    return {"message": "Image sharpened successfully", "sharpened_path": 'http://127.0.0.1:8000/output/' + folder_name + '/sharpened.jpg'}
+
+@app.get("/results")
+def get_results():
+    # Get a list of all subdirectories in the output directory
+    folders = [f for f in os.listdir('./output') if os.path.isdir(os.path.join('./output', f))]
+    return {"results": folders}
+
+@app.delete('/results/{folder_name}')
+def delete_result(folder_name):
+    # Delete the folder with the given name
+    try: 
+        os.rmdir('./output/' + folder_name)
+        return {"message": "Folder deleted successfully"}
+    except OSError as e:
+        return Response(
+            status_code=404,
+            content="Folder not found",
+        )
+
+# python -m uvicorn app:app --reload
